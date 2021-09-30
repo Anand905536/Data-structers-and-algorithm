9496b2dece8c8304f12394b236e7ff722b88a6c5
Anand905536 <74814275+Anand905536@users.noreply.github.com>
2021-09-30 13:44:42 -0500

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,470 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define  u 78
+// void Q(int z)
+// {
+//   z += z;
+//   cout<<z << " ";
+// }
+
+// void P(int *y) 
+// {
+//   int x = *y + 2;
+
+//   Q(x);
+//   *y = x - 1; 
+//   cout<<x << " ";
+// }
+  // void f(int x, int *py, int **ppz) {
+  //   int y, z;
+  //   **ppz += 1;
+  //   z = **ppz;
+  //   *py += 2;
+  //   y = *py;
+  //   x += 3;
+  //   cout<< x + y + z;
+//}
+// void func(int p){
+
+//     cout << p << " ";
+// }
+int main(){
+
+int n;
+cin>>n;
+cout<<(n+u)<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // int c, *b, **a;
+  //   c = 4;
+  //   b = &c;
+  //   a = &b;
+  // f(c, b, a);
+
+
+
+
+
+
+
+
+// int i = 10;
+//     int &p = i;
+//     func(p++);
+//     cout << i;
+
+
+
+
+
+
+
+
+
+ // int ***r, **q, *p, i=8;
+ //  p = &i;
+ //  (*p)++;
+ //  q = &p;
+ //  (**q)++;
+ //  r = &q;
+ //  cout<<*p << " " <<**q << " "<<***r<<" "<<**r<<" "<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // int x = 5;
+ //  P(&x);
+ //  cout<<x;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//char st[] = "ABCD";
+  // char st='A';
+  // // for(int i = 0; st[i] != '\0'; i++) {
+  //    // cout << st[i] << *(st)+i << *(i+st) << i[st];
+    // cout<<st+0<<endl;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //  float arr[5] = {12.5, 10.0, 13.5, 90.5, 0.5};
+  // float *ptr1 = &arr[0];
+  // float *ptr2 = ptr1 + 3;
+  // cout<<*ptr2<<" ";
+  // cout<< ptr2 - ptr1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   int numbers[5]={1,2,3,4,5};
+//   int * p;
+//   p = numbers; 
+//   *p = 10;
+//   p = &numbers[2]; 
+//   *p = 20;
+//   p--; 
+//   *p = 30;
+//   p = numbers + 3;
+// *p = 40;
+//   p = numbers;
+
+//   *(p+4) = 50;
+//   for (int n=0; n<5; n++) {
+//     cout << numbers[n] << ",";
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	 int arr[] = {4, 5, 6, 7};
+//   int *p = (arr + 1);
+//   cout << *(arr+1)+ 9;
+// //  int a = 10;
+// int *p = &a;
+// int **q = &p;
+//  int b = 20;
+//  *q = &b;
+//  cout<<&b<<endl;
+// cout<<*q<<endl;
+// cout<<p<<endl;
+// (*p)++;
+// cout << a << " " << b << endl;
+	 // int a = 100;
+  // int *p = &a;
+  // int **q = &p;
+  // int b = (**q)++;
+  // int *r = *q;
+  // (*r)++;
+  // cout << a << " " << b << endl;
+
+ return 0;
+}
